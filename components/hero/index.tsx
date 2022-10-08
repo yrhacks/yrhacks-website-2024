c4a0667a829d3253caffd7f61c4b4583a62a82dc
RZ-Music <ryanzhu2005@gmail.com>
2022-10-08 16:37:43 -0400

    Gradients

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -2,9 +2,13 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <header className="w-screen h-screen bg-slate-950">
-      <h1>YRHacks</h1>
-      <h2>A YRDSB Hackathon run by students for students!</h2>
+    <header className="w-screen h-screen flex flex-col justify-center items-center bg-slate-950 px-5">
+      <h1 className="w-full text-5xl font-bold mb-10">YRHacks</h1>
+      <h3 className="w-full text-xl font-semibold">
+        A YRDSB <span className="gradient-purple">Hackathon</span> run by{" "}
+        <span className="gradient-pumpkin">students</span> for{" "}
+        <span className="gradient-teal">students</span>!
+      </h3>
     </header>
   );
 };
