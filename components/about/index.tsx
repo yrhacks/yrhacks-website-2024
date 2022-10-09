c00435f788d1025ddc33d9996acbf4f95c2775dc
RZ-Music <ryanzhu2005@gmail.com>
2022-10-09 14:57:34 -0400

    Colours gradient

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -4,14 +4,22 @@ const About = () => {
   return (
     <main
       id="about"
-      className="w-screen pt-36 pb-48 flex flex-col justify-center items-start bg-slate-50 px-5"
+      className="w-screen pt-36 pb-48 flex flex-col justify-center items-start bg-slate-950 px-5 md:px-10 lg:px-48"
     >
-      <h2 className="text-2xl text-slate-950 font-bold">
-        YRHacks is a YRDSB <span className="gradient-teal text-transparent !bg-clip-text font-bold">Hackathon</span> run by students for students.
+      <h2 className="text-2xl text-slate-50 font-bold">
+        YRHacks is a YRDSB{" "}
+        <span className="gradient-teal text-transparent !bg-clip-text font-bold">
+          Hackathon
+        </span>{" "}
+        run by students, for students.
       </h2>
 
-      <p className="text-slate-950 font-medium mt-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, iusto. Fugit facere consequatur maxime unde quasi, atque deleniti ab blanditiis</p>
-    </div>
+      <p className="text-slate-950 font-medium mt-8">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
+        iusto. Fugit facere consequatur maxime unde quasi, atque deleniti ab
+        blanditiis
+      </p>
+    </main>
   );
 };
 
--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import emailjs from "emailjs-com";
+import { BsInstagram } from "react-icons/bs";
 
 const initialState = {
   name: "",
@@ -46,47 +47,62 @@ const Contact = () => {
     >
       <h2 className="text-4xl font-semibold text-white mb-8">Contact</h2>
       <h4 className="mb-3 text-md">Get In Touch</h4>
-      <form
-        onSubmit={handleSubmit}
-        className="w-full flex flex-col justify-start items-start"
-        name="form"
-        autoComplete="off"
-      >
-        <input
-          type="text"
-          id="name"
-          name="name"
-          className="border-slate-400 border-2 p-3 bg-zinc-800 w-full mb-3 text-sm"
-          placeholder="Full Name"
-          required
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          id="email"
-          name="email"
-          className="border-slate-400 border-2 p-3 bg-zinc-800 w-full mb-3 text-sm"
-          placeholder="Email"
-          required
-          onChange={handleChange}
-        />
-        <textarea
-          name="message"
-          id="message"
-          className="border-slate-400 border-2 p-3 bg-zinc-800 w-full mb-3 text-sm"
-          rows={4}
-          placeholder="Message"
-          required
-          onChange={handleChange}
-        ></textarea>
-        <button
-          type="submit"
-          className="bg-slate-400 p-3 text-white font-semibold"
-          disabled={submitted}
+      <div>
+        <form
+          onSubmit={handleSubmit}
+          className="w-full flex flex-col justify-start items-start"
+          name="form"
+          autoComplete="off"
         >
-          {submitted ? "Sent!" : "Send"}
-        </button>
-      </form>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="border-slate-400 border-2 p-3 bg-zinc-800 w-full mb-3 text-sm"
+            placeholder="Full Name"
+            required
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            id="email"
+            name="email"
+            className="border-slate-400 border-2 p-3 bg-zinc-800 w-full mb-3 text-sm"
+            placeholder="Email"
+            required
+            onChange={handleChange}
+          />
+          <textarea
+            name="message"
+            id="message"
+            className="border-slate-400 border-2 p-3 bg-zinc-800 w-full mb-3 text-sm"
+            rows={4}
+            placeholder="Message"
+            required
+            onChange={handleChange}
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-slate-400 p-3 text-white font-semibold"
+            disabled={submitted}
+          >
+            {submitted ? "Sent!" : "Send"}
+          </button>
+        </form>
+        <div>
+          <div className="flex flex-row justify-start items-center">
+            <BsInstagram />
+            <a
+              href="https://www.instagram.com/yrhacks/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white ml-2"
+            >
+              @yrhacks
+            </a>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
