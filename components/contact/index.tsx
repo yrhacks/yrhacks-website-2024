fe8ade6601fa9986cba229d688160e8fd00890ad
RZ-Music <ryanzhu2005@gmail.com>
2022-10-09 21:04:43 -0400

    Fix mobile gradient

--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -44,78 +44,83 @@ const Contact = () => {
   return (
     <main
       id="contact"
-      className="bg-slate-950 w-screen h-screen flex flex-col justify-center items-start px-5 md:px-10 lg:px-48"
+      className="relative bg-slate-950 w-screen h-screen flex flex-col justify-center items-start px-5 md:px-10 lg:px-48"
     >
-      <h2 className="text-4xl font-semibold text-slate-50 mb-5 font-helvetica">
-        Contact
-      </h2>
-      <h4 className="mb-4 text-lg font-normal text-slate-50">Get In Touch!</h4>
-      <div className="w-full flex flex-col lg:flex-row justify-between items-start">
-        <form
-          onSubmit={handleSubmit}
-          className="w-full lg:w-3/5 flex flex-col justify-start items-start md:mb-10"
-          name="form"
-          autoComplete="off"
-        >
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="border-slate-50 text-gray-700 border-2 rounded-none p-3 bg-transparent w-full mb-3 text-sm duration-500  hover:bg-zinc-800/80 focus:bg-zinc-700/80"
-            placeholder="Full Name"
-            required
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            id="email"
-            name="email"
-            className="border-slate-50 text-gray-700 border-2 rounded-none p-3 bg-transparent w-full mb-3 text-sm duration-500  hover:bg-zinc-800/80 focus:bg-zinc-700/80"
-            placeholder="Email"
-            required
-            onChange={handleChange}
-          />
-          <textarea
-            name="message"
-            id="message"
-            className="border-slate-50 text-gray-700 border-2 rounded-none p-3 bg-transparent w-full mb-3 text-sm duration-500 hover:bg-zinc-800/80 focus:bg-zinc-700/80"
-            rows={4}
-            placeholder="Message"
-            required
-            onChange={handleChange}
-          />
-          <button
-            type="submit"
-            className="border-2 border-slate-50 duration-700 transition-all ease-in-out hover:bg-slate-50 "
-            disabled={submitted}
+      <div className="absolute bottom-16 right-16 lg:right-72 w-52 h-52 rounded-tr-md rounded-bl-md rounded-tl-full rounded-br-full bg-gradient-to-br from-blue-700/80 to-purple-300/80 blur-[150px] z-10" />
+      <div className="z-20 w-full">
+        <h2 className="text-4xl font-semibold text-slate-50 mb-5 font-helvetica">
+          Contact
+        </h2>
+        <h4 className="mb-4 text-lg font-normal text-slate-50">
+          Get In Touch!
+        </h4>
+        <div className="w-full flex flex-col lg:flex-row justify-between items-start">
+          <form
+            onSubmit={handleSubmit}
+            className="w-full lg:w-3/5 flex flex-col justify-start items-start md:mb-10"
+            name="form"
+            autoComplete="off"
           >
-            <p className="px-6 py-3 duration-700 transition-all ease-in-out text-slate-50 !bg-clip-text hover:text-transparent gradient-purple font-semibold cursor-pointer">
-              {submitted ? "Sent!" : "Send"}
-            </p>
-          </button>
-        </form>
-        <div className="w-full lg:w-2/5 pt-8 md:pt-0 md:px-8">
-          <div className="flex flex-row justify-start items-center mb-5">
-            <a
-              href="https://www.instagram.com/yrhacks/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-slate-50 ml-2 text-sm md:text-lg flex duration-300 ease-in-out transition-all hover:text-transparent gradient-purple !bg-clip-text"
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="border-slate-50 text-gray-700 border-2 rounded-none p-3 bg-transparent w-full mb-3 text-sm duration-500  hover:bg-zinc-800/80 focus:bg-zinc-700/80"
+              placeholder="Full Name"
+              required
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className="border-slate-50 text-gray-700 border-2 rounded-none p-3 bg-transparent w-full mb-3 text-sm duration-500  hover:bg-zinc-800/80 focus:bg-zinc-700/80"
+              placeholder="Email"
+              required
+              onChange={handleChange}
+            />
+            <textarea
+              name="message"
+              id="message"
+              className="border-slate-50 text-gray-700 border-2 rounded-none p-3 bg-transparent w-full mb-3 text-sm duration-500 hover:bg-zinc-800/80 focus:bg-zinc-700/80"
+              rows={4}
+              placeholder="Message"
+              required
+              onChange={handleChange}
+            />
+            <button
+              type="submit"
+              className="border-2 border-slate-50 duration-700 transition-all ease-in-out hover:bg-slate-50 "
+              disabled={submitted}
             >
-              <FaInstagram className="text-slate-50 text-xl md:text-3xl mr-3" />
-              @yrhacks
-            </a>
-          </div>
-          <div className="flex flex-row justify-start items-center">
-            <a
-              href="mailto:yorkregionhacks@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-slate-50 ml-2 text-sm md:text-lg flex duration-300 ease-in-out transition-all hover:text-transparent gradient-purple !bg-clip-text"
-            >
-              <AiOutlineMail className="text-slate-50 text-xl md:text-3xl mr-3" />
-              yorkregionhacks@gmail.com
-            </a>
+              <p className="px-6 py-3 duration-700 transition-all ease-in-out text-slate-50 !bg-clip-text hover:text-transparent gradient-purple font-semibold cursor-pointer">
+                {submitted ? "Sent!" : "Send"}
+              </p>
+            </button>
+          </form>
+          <div className="w-full lg:w-2/5 pt-8 md:pt-0 md:px-8">
+            <div className="flex flex-row justify-start items-center mb-5">
+              <a
+                href="https://www.instagram.com/yrhacks/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-slate-50 ml-2 text-sm md:text-lg flex duration-300 ease-in-out transition-all hover:text-transparent gradient-purple !bg-clip-text"
+              >
+                <FaInstagram className="text-slate-50 text-xl md:text-3xl mr-3" />
+                @yrhacks
+              </a>
+            </div>
+            <div className="flex flex-row justify-start items-center">
+              <a
+                href="mailto:yorkregionhacks@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-slate-50 ml-2 text-sm md:text-lg flex duration-300 ease-in-out transition-all hover:text-transparent gradient-purple !bg-clip-text"
+              >
+                <AiOutlineMail className="text-slate-50 text-xl md:text-3xl mr-3" />
+                yorkregionhacks@gmail.com
+              </a>
+            </div>
           </div>
         </div>
       </div>
--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -39,35 +39,37 @@ const Hero = () => {
 
   return (
     <header className="w-screen h-screen flex flex-col justify-center items-start bg-slate-950 pt-10 px-5 md:px-10 lg:px-48">
-      <div className="absolute bottom-0 left-0 w-48 h-48 lg:w-80 lg:h-80 rounded-tr-full rounded-bl-full rounded-tl-md rounded-br-md bg-rose-600/50 blur-[100px] lg:blur-[150px] z-10" />
-      <div className="absolute top-32 lg:top-64 right-24 w-48 h-48 lg:w-64 lg:h-64 rounded-tr-full rounded-bl-full rounded-tl-md rounded-br-md bg-blue-600/50 blur-[100px] lg:blur-[130px] z-10" />
-      <p className="text-sm font-semibold text-slate-50/50">SPRING 2023</p>
-      <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold my-1 font-helvetica text-slate-50">
-        YRHacks
-      </h1>
-      <p className="text-xl font-semibold italic mb-4 text-slate-50">
-        {begun ? "has begun!" : "is in..."}
-      </p>
-      <Countdown
-        days={days}
-        hours={hours}
-        minutes={minutes}
-        seconds={seconds}
-      />
-      <p className="text-sm lg:text-xl mt-8 text-slate-50">
-        Registrations for YRHacks 2023 will open in January 2023. Stay tuned!
-      </p>
-      <div className="flex flex-row items-center w-full mt-6">
-        <MdLocationOn className="text-xl lg:text-2xl mr-2 text-slate-50" />
-        <p className="text-sm lg:text-lg font-semibold text-slate-50">
-          Richmond Green S.S. (+Virtual)
+      <div className="absolute bottom-0 left-0 w-48 h-48 lg:w-80 lg:h-80 rounded-tr-full rounded-bl-full rounded-tl-md rounded-br-md bg-rose-600/50 blur-[150px] z-10" />
+      <div className="absolute top-32 lg:top-64 right-24 w-48 h-48 lg:w-64 lg:h-64 rounded-tr-full rounded-bl-full rounded-tl-md rounded-br-md bg-blue-600/50 blur-[130px] z-10" />
+      <div className="z-20 w-full">
+        <p className="text-sm font-semibold text-slate-50/50">SPRING 2023</p>
+        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold my-1 font-helvetica text-slate-50">
+          YRHacks
+        </h1>
+        <p className="text-xl font-semibold italic mb-4 text-slate-50">
+          {begun ? "has begun!" : "is in..."}
         </p>
-      </div>
-      <div className="flex flex-row items-center w-full mt-3">
-        <MdCalendarToday className="text-xl lg:text-2xl mr-2 text-slate-50" />
-        <p className="text-sm lg:text-lg font-semibold text-slate-50">
-          March 3-4 2023
+        <Countdown
+          days={days}
+          hours={hours}
+          minutes={minutes}
+          seconds={seconds}
+        />
+        <p className="text-sm lg:text-xl mt-8 text-slate-50">
+          Registrations for YRHacks 2023 will open in January 2023. Stay tuned!
         </p>
+        <div className="flex flex-row items-center w-full mt-6">
+          <MdLocationOn className="text-xl lg:text-2xl mr-2 text-slate-50" />
+          <p className="text-sm lg:text-lg font-semibold text-slate-50">
+            Richmond Green S.S. (+Virtual)
+          </p>
+        </div>
+        <div className="flex flex-row items-center w-full mt-3">
+          <MdCalendarToday className="text-xl lg:text-2xl mr-2 text-slate-50" />
+          <p className="text-sm lg:text-lg font-semibold text-slate-50">
+            March 3-4 2023
+          </p>
+        </div>
       </div>
     </header>
   );
