4803c847fe0350050e8952797bbab9554e9127b2
RZ-Music <ryanzhu2005@gmail.com>
2023-02-25 13:51:10 -0500

    update countdown end

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -9,34 +9,39 @@ const Hero = () => {
   const [minutes, setMinutes] = useState<number>(0);
   const [seconds, setSeconds] = useState<number>(0);
   const [begun, setBegun] = useState(false);
+  const [ended, setEnded] = useState(false);
 
   // launch date: March 03, 2023
-  const launchDate = new Date("2023-03-04T23:00:00Z").getTime();
-
+  let launchDate = new Date("2023-03-04T23:00:00Z").getTime();
   useEffect(() => {
     const updateTime = setInterval(() => {
       const now = new Date().getTime();
-      const until = launchDate - now;
-
-      setDays(Math.floor(until / (1000 * 60 * 60 * 24)));
-      setHours(Math.floor((until % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-      setMinutes(Math.floor((until % (1000 * 60 * 60)) / (1000 * 60)));
-      setSeconds(Math.floor((until % (1000 * 60)) / 1000));
+      let until = launchDate - now;
 
       if (until <= 0) {
+        launchDate = new Date("2023-03-06T02:00:00Z").getTime();
+        setBegun(true);
+      }
+      until = launchDate - now;
+      if (begun && until <= 0) {
         clearInterval(updateTime);
         setDays(0);
         setHours(0);
         setMinutes(0);
         setSeconds(0);
-        setBegun(true);
+        setEnded(true);
+        return;
       }
+      setDays(Math.floor(until / (1000 * 60 * 60 * 24)));
+      setHours(Math.floor((until % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+      setMinutes(Math.floor((until % (1000 * 60 * 60)) / (1000 * 60)));
+      setSeconds(Math.floor((until % (1000 * 60)) / 1000));
     });
 
     return () => {
       clearInterval(updateTime);
     };
-  }, [launchDate]);
+  }, [seconds]);
 
   return (
     <header className="relative w-screen h-screen flex flex-col justify-center items-start bg-transparent pt-20 px-5 md:px-10 lg:px-48">
@@ -57,27 +62,30 @@ const Hero = () => {
           data-aos="fade-up"
           className="text-xl font-semibold italic mb-4 text-violet-100/80"
         >
-          {begun ? "has begun!" : "is in..."}
+          {begun ? (ended ? "has ended!" : "ends in...") : "is in..."}
         </p>
-        <Countdown
-          days={days}
-          hours={hours}
-          minutes={minutes}
-          seconds={seconds}
-        />
+        {!ended && (
+          <Countdown
+            days={days}
+            hours={hours}
+            minutes={minutes}
+            seconds={seconds}
+          />
+        )}
         <div
           data-aos="fade-up"
           data-aos-delay="700"
           className="flex flex-col justify-start items-start"
         >
-          <div className="gradient-purple rounded-full w-60 px-8 py-3 flex items-center justify-center cursor-pointer">
+          <div className="gradient-purple rounded-full px-8 py-3 flex items-center justify-center">
             <p className="text-sm lg:text-lg text-violet-100/80 font-bold text-center">
-              Registration is now closed!
+              {begun
+                ? ended
+                  ? "Thank you for attending! See you next year!"
+                  : "YRHacks has begun!"
+                : "Registration is now closed!"}
             </p>
           </div>
-          <p className="mt-4 text-violet-100/80 text-md text-lg">
-            See you there!
-          </p>
           <div className="flex flex-row items-center w-full mt-6">
             <MdLocationOn className="text-xl lg:text-2xl mr-2 text-violet-100/80" />
             <p className="text-sm lg:text-lg font-semibold text-violet-100/80">
