f457d72adc0508f7c3d3354350dce80bc25270cf
greymesa <58270183+greymesa@users.noreply.github.com>
2024-02-20 10:16:48 -0500

    made it more visible

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -70,7 +70,7 @@ const Hero = () => {
       // Before Hackathon Start
       if (registrationOpen) {
         // After Registration Start
-        return <a href="https://docs.google.com/forms/d/e/1FAIpQLSf84vdRChXpE1mara1uxOYkrqBTElCejCbXvISNwqNrAW1O4g/viewform?usp=sf_link">Registration is now open!</a>;
+        return <a href="https://docs.google.com/forms/d/e/1FAIpQLSf84vdRChXpE1mara1uxOYkrqBTElCejCbXvISNwqNrAW1O4g/viewform?usp=sf_link">Registration is now open! (Click for registration form!)</a>;
       } else {
         if (registrationSoon) {
           // Before Registration Start Soon
